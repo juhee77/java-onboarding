ddce89c77e2427e2b55c68694e5bed60635c5f1e
lahezy <jouhi3322@gmail.com>
2022-12-15 23:22:40 +0900

    feat : 6번 문제 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        HashSet<String> result = new HashSet<>();
+        HashMap<String, String> allNameSet = new HashMap<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0).trim();
+            String name = form.get(1);
+            for (int i = 0; i < name.length() - 1; i++) {
+                String subName = name.substring(i, i + 2);
+                if (allNameSet.containsKey(subName)) {
+                    result.add(email);
+                    result.add(allNameSet.get(subName));
+                }
+                allNameSet.put(subName, email);
+            }
+        }
+
+        return result.stream().sorted().collect(Collectors.toList());
     }
 }
