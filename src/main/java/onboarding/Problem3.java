cb3a4a86c994aa1cf67dfe3c09395d7992282457
lahezy <jouhi3322@gmail.com>
2022-12-15 22:48:57 +0900

    feat : 3번 문제 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int clap = 0;
+        for (int num = 1; num <= number; num++) {
+            clap += countClaps(num);
+        }
+        return clap;
+    }
+
+    private static int countClaps(int num) {
+        int claps = 0;
+        while (0 < num) {
+            int eachNum = num % 10;
+            if (eachNum == 3 || eachNum == 6 || eachNum == 9) {
+                claps++;
+            }
+            num /= 10;
+        }
+        return claps;
     }
 }
